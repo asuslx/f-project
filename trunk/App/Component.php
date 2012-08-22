1c91da65ebedaf5b211c56b7edf5b7a18287a4d8
asuslx@gmail.com <asuslx@gmail.com@222bc962-5a08-8ca2-82db-426d0ecb96ec>
2012-08-22 13:31:31 +0000

    finished frame and component

--- trunk/App/Component.php
+++ trunk/App/Component.php
@@ -4,6 +4,32 @@
  * Date: 4/26/12
  */
  
-class F_App_Component {
-    
+abstract class F_App_Component {
+
+    private $_template;
+
+    public function __construct() {
+
+        $file = APP_ROOT . 'tmpl/' . str_replace('_', '/', get_class($this)) . '.html';
+        $this->_template = $this->getTemplate($file);
+    }
+
+    protected function _assign($name, $value) {
+
+        $this->_template->assign($name, $value);
+    }
+
+    protected function getTemplate($file) {
+
+        return new F_App_Template_Native($file);
+    }
+
+    protected abstract function _run();
+
+    public function run() {
+
+        $this->_run();
+        return $this->_template->parse();
+    }
+
 }
--- trunk/App/Frame.php
+++ trunk/App/Frame.php
@@ -4,29 +4,6 @@
  * Date: 4/26/12
  */
  
-class F_App_Frame {
-
-    private $_template;
-
-    public function __construct() {
-
-        $file = APP_ROOT . 'tmpl/' . str_replace('_', '/', get_class($this)) . '.html';
-        $this->_template = $this->getTemplate($file);
-    }
-
-    protected function _assign($name, $value) {
-
-        $this->_template->assign($name, $value);
-    }
-
-    protected function getTemplate($file) {
-
-        return new F_App_Template_Native($file);
-    }
-
-    public function run() {
-
-        return $this->_template->parse();
-    }
+abstract class F_App_Frame extends F_App_Component {
 
 }
